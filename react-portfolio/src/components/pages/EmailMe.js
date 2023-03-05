7c6316844b4452b713ac88cfa5e353a71ce6c520
AdibaSjd <adibasjd.id@gmail.com>
2023-03-05 22:01:29 +0000

    removed email.js

--- react-portfolio/src/components/pages/EmailMe.js
+++ react-portfolio/src/components/pages/EmailMe.js
@@ -1,21 +1,11 @@
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
 
 const EmailMe = () => {
   const form = useRef(null);
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_j9rvnq7', 'template_mwtvdb5', form.current, '1oRoXqh0YGd5vApp8')
-      .then((result) => {
-          console.log(result.text);
-          form.current.reset();
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
-
+  }
   return (
     <div className="email">
     <form ref={form} onSubmit={sendEmail}>
