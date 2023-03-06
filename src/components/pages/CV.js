ae4ed65bea4aa3abe182226b92ac77eff064eda7
AdibaSjd <adibasjd.id@gmail.com>
2023-03-06 22:49:05 +0000

    download cv

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -1,107 +1,126 @@
+
 import React from 'react';
+import Resume from "../pages/images/CV-Adiba-Sajid.pdf";
 
-export default function Blog() {
-  return (
+function downloadResume() {
 
-<div>
-  <div className='CV'> 
-
-
-<div>
-<h1> ADIBA FATIMA SAJID </h1>
-
-
-<div>
-<h2>PERSONAL PROFILE </h2>
-
-I’m a highly motivated individual with an optimistic view and positive attitude towards all aspects of work. 
-I’m highly reliable with extensive experience in working with a diverse range of people. I take pride in being well organised, 
-managing my time, and tackling difficult situations in an appropriate manner; always aiming to work consistently to the highest of my standards.
-</div>
-</div>
-</div>
-
-<div className='skills'>
-<ul>
-Customer based:
-<li>Excellent interpersonal, communication & relationship building skills</li>
-<li>Prioritising customer care and experience</li>
-<li>Bilingual; Fluent in English, Arabic & Darija Arabic</li>
-</ul>
-
-<ul>Computer based:
-<li>Quick & accurate keyboard skills</li> 
-<li>Microsoft: Excel, PowerPoint, Word</li>
-<li>Photoshop & InDesign</li>
-<li>iOS and MacOS X systems</li>
-<li>HTML5, CSS3, JavaScript,
-jQuery, Node.js, word press, OOP, SQL, React and more
-</li>
-</ul>
-
-<ul>
-  Personal:
-  <li>Ability to work on tasks individually & as part of a team</li>
-  <li>Problem solving; understanding problems & developing inventive solutions</li>
-  <li>Ability to focus on accuracy and attention to detail whilst under pressure & against deadlines</li>
-</ul>
-</div>
-
-<div className='job'>
-<h2>Work Experience</h2>
-<h3>Customer advisor - HSBC, Birmingham (2022-2022)</h3>
-<ul>
-  <li>Building customer relationships</li>
-  <li>Managing a high volume of customer queries</li>
-  <li> Following mandatory security protocols</li>
-  <li>Prioritising customers experience and analysing feedback. Making sure each customer's experience is better each time</li>
-  <li>Helping a diverse range of people</li>
-  <li>Ability to multitask between systems</li>
-  <li>Learning and implementing programs</li>
-  <li>Confidence in rapport building with customers while remaining professional</li>
-</ul>
-
-<h3>Customer advisor - Esure, Birmingham (2022-2022)</h3>
-<ul>
-  <li>Effective interpersonal skills</li>
-  <li>Handling emails to and from customers in a professional environment while solving enquires</li>
-  <li>Handing inbound and outbound calls</li>
-  <li>Learning vast amounts of information and multitasking between computer systems</li>
-  <li>Solid Insurance, Finacial and legal knowledge</li>
-  <li>Strong team working skills while working between different areas of the company to resolve issues fast and effectively</li>
-</ul>
-
-<h3>Sales Associate - Victoria secret, Birmingham (2021-2022)</h3>
-<ul>
-  <li>Meeting sale targets</li>
-  <li>Managing floors and tills</li>
-  <li>Organized under a constant heavy work load</li>
-  <li>Creative thinking when assisting customers finding suitable products and services</li>
-  <li>Enhanced upselling skills</li>
-  <li>Helping a diverse range of customers making them feel comfortable in a personal environment</li>
-  <li>Being flexible and understanding to each individual need</li>
-  <li>Handling payments, processing returns and refunds</li>
-</ul>
-
-<h3>Admin/Sales Associate/Social Media Manager - Icon Grillz Jeweller's, Jewellery Quarter, Birmingham (2018-2020)</h3>
-<ul>
-  <li>Book keeping/Sorting finances</li>
-  <li>Professionalism when answering customer inquiries face to face, phone, social media and emails</li>
-  <li>Events management – Scheduling public events</li>
-  <li>Working strictly to companies' protocols in a professional environment to ensure everyone's safety</li>
-  <li>resolving customer queries at first point of contact</li>
-  <li>Receiving and sending out orders on time</li>
-  <li>Product/brand knowledge around all types of Jewlery and qualities</li>
-  <li>Ordering and processing materials needed</li>
-</ul>
-
-</div>
-
-
-
-
-
-
-</div>
-  );
+return (
+    <div className='Button'>
+    <a style={{borderColor:'white', borderWidth:'2px', backgroundColor:'rgb(48, 46, 46)'}} href={Resume} download="Adiba-Sajid-resume.pdf" type="button"  class="btn btn-secondary">Download Resume</a>
+    </div>
+)
 }
+
+export default downloadResume;
+
+
+
+
+
+// import React from 'react';
+
+// export default function Blog() {
+//   return (
+
+// <div>
+//   <div className='CV'> 
+
+
+// <div>
+// <h1> ADIBA FATIMA SAJID </h1>
+
+
+// <div>
+// <h2>PERSONAL PROFILE </h2>
+
+// I’m a highly motivated individual with an optimistic view and positive attitude towards all aspects of work. 
+// I’m highly reliable with extensive experience in working with a diverse range of people. I take pride in being well organised, 
+// managing my time, and tackling difficult situations in an appropriate manner; always aiming to work consistently to the highest of my standards.
+// </div>
+// </div>
+// </div>
+
+// <div className='skills'>
+// <ul>
+// Customer based:
+// <li>Excellent interpersonal, communication & relationship building skills</li>
+// <li>Prioritising customer care and experience</li>
+// <li>Bilingual; Fluent in English, Arabic & Darija Arabic</li>
+// </ul>
+
+// <ul>Computer based:
+// <li>Quick & accurate keyboard skills</li> 
+// <li>Microsoft: Excel, PowerPoint, Word</li>
+// <li>Photoshop & InDesign</li>
+// <li>iOS and MacOS X systems</li>
+// <li>HTML5, CSS3, JavaScript,
+// jQuery, Node.js, word press, OOP, SQL, React and more
+// </li>
+// </ul>
+
+// <ul>
+//   Personal:
+//   <li>Ability to work on tasks individually & as part of a team</li>
+//   <li>Problem solving; understanding problems & developing inventive solutions</li>
+//   <li>Ability to focus on accuracy and attention to detail whilst under pressure & against deadlines</li>
+// </ul>
+// </div>
+
+// <div className='job'>
+// <h2>Work Experience</h2>
+// <h3>Customer advisor - HSBC, Birmingham (2022-2022)</h3>
+// <ul>
+//   <li>Building customer relationships</li>
+//   <li>Managing a high volume of customer queries</li>
+//   <li> Following mandatory security protocols</li>
+//   <li>Prioritising customers experience and analysing feedback. Making sure each customer's experience is better each time</li>
+//   <li>Helping a diverse range of people</li>
+//   <li>Ability to multitask between systems</li>
+//   <li>Learning and implementing programs</li>
+//   <li>Confidence in rapport building with customers while remaining professional</li>
+// </ul>
+
+// <h3>Customer advisor - Esure, Birmingham (2022-2022)</h3>
+// <ul>
+//   <li>Effective interpersonal skills</li>
+//   <li>Handling emails to and from customers in a professional environment while solving enquires</li>
+//   <li>Handing inbound and outbound calls</li>
+//   <li>Learning vast amounts of information and multitasking between computer systems</li>
+//   <li>Solid Insurance, Finacial and legal knowledge</li>
+//   <li>Strong team working skills while working between different areas of the company to resolve issues fast and effectively</li>
+// </ul>
+
+// <h3>Sales Associate - Victoria secret, Birmingham (2021-2022)</h3>
+// <ul>
+//   <li>Meeting sale targets</li>
+//   <li>Managing floors and tills</li>
+//   <li>Organized under a constant heavy work load</li>
+//   <li>Creative thinking when assisting customers finding suitable products and services</li>
+//   <li>Enhanced upselling skills</li>
+//   <li>Helping a diverse range of customers making them feel comfortable in a personal environment</li>
+//   <li>Being flexible and understanding to each individual need</li>
+//   <li>Handling payments, processing returns and refunds</li>
+// </ul>
+
+// <h3>Admin/Sales Associate/Social Media Manager - Icon Grillz Jeweller's, Jewellery Quarter, Birmingham (2018-2020)</h3>
+// <ul>
+//   <li>Book keeping/Sorting finances</li>
+//   <li>Professionalism when answering customer inquiries face to face, phone, social media and emails</li>
+//   <li>Events management – Scheduling public events</li>
+//   <li>Working strictly to companies' protocols in a professional environment to ensure everyone's safety</li>
+//   <li>resolving customer queries at first point of contact</li>
+//   <li>Receiving and sending out orders on time</li>
+//   <li>Product/brand knowledge around all types of Jewlery and qualities</li>
+//   <li>Ordering and processing materials needed</li>
+// </ul>
+
+// </div>
+
+
+
+
+
+
+// </div>
+//   );
+// }
